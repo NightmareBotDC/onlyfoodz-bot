a1bdfac857c7eae3ba889c9a94e0d5795a6aeb35
selectdev <66577268+selectdev@users.noreply.github.com>
2023-02-24 18:32:48 -0600

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,42 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("ping")
+		.setDescription("Check the bot's ping"),
+	async execute(client, interaction, database, voice) {
+		const reply = await interaction.reply({
+			content: "",
+			embeds: [
+				new client.EmbedBuilder()
+					.setColor("Orange")
+					.setDescription(
+						`Checking Discord Websocket Latency & Discord Interaction Roundtrip Latency...`
+					),
+			],
+			fetchReply: true,
+		});
+
+		const interactionLatency = Math.round(
+			reply.createdTimestamp - interaction.createdTimestamp
+		);
+
+		reply.edit({
+			embeds: [
+				new client.EmbedBuilder().setColor("Blue").addFields(
+					{
+						name: `Discord Websocket Latency`,
+						value: `\`${interaction.client.ws.ping}\`ms`,
+						inline: true,
+					},
+					{
+						name: `Discord Interaction Roundtrip Latency`,
+						value: `\`${interactionLatency}\`ms`,
+						inline: true,
+					}
+				),
+			],
+		});
+	},
+	async autocomplete(client, interaction, database) {},
+};
